7bb93567249388c3630770f3d178d9ebca7ace51
Briana Oniru <damioniru16@gmail.com>
2020-08-20 17:04:20 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+def reverse_each_word(string)
+   revrs_string = []
+  string.split.each do |sent|
+    revrs_string << sent.reverse
+ end
+  revrs_string.join(" ")
+end
+
+def reverse_each_word(string)
+  string.split.collect do |sentence|
+    sentence.reverse
+ end
+end
